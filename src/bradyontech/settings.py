66c0718b231192dc3040f78b155d8e2a19582997
SlouchyBeanie <brady.anderson27@gmail.com>
2019-04-30 18:00:07 -0600

    Updated settings with python-decouple for environment variables for easier development/production code migration

--- src/bradyontech/settings.py
+++ src/bradyontech/settings.py
@@ -12,21 +12,21 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 from decouple import config
+from decouple import Csv
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-with open('env/secret_key.txt') as f:
-	SECRET_KEY = f.read().strip()
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['www.bradyontech.com', 'bradyontech.com']
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 #Sitemap
 SITE_ID = 1
@@ -119,12 +119,12 @@ HAYSTACK_CONNECTIONS = {
 # Database
 DATABASES = {
     'default': {
-	'ENGINE': 'django.db.backends.postgresql_psycopg2',
+	'ENGINE': config('DB_ENGINE'),
 	'NAME': config('DB_NAME'),
 	'USER': config('DB_USER'),
 	'PASSWORD': config('DB_PASS'),
-	'HOST': '',
-	'PORT': '',
+	'HOST': config('DB_HOST'),
+	'PORT': config('DB_PORT'),
     }
 }
 
