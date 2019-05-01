db1f20f9f17bb2e78f3780b079c48027910a100b
SlouchyBeanie <brady.anderson27@gmail.com>
2019-05-01 07:47:26 -0600

    Kept SQLite3 db configs for local dev

--- src/bradyontech/settings.py
+++ src/bradyontech/settings.py
@@ -115,6 +115,13 @@ HAYSTACK_CONNECTIONS = {
     },
 }
 
+# SQLite3
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
 
 # Database
 DATABASES = {
